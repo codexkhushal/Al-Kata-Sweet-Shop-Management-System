0c8d144c2a35e7afe9ec0345b48d90441602b4e9
codexkhushal <khushal.sharma7493@gmail.com>
2025-12-14 16:30:36 +0530

    CHORE: Initialize Spring Boot backend and configure base application file.

---
+++ backend/src/main/java/com/codexkhushal/Al/Kata/Sweet/Shop/Managment/System/AlKataSweetShopManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.codexkhushal.Al.Kata.Sweet.Shop.Managment.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlKataSweetShopManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlKataSweetShopManagmentSystemApplication.class, args);
+	}
+
+}
